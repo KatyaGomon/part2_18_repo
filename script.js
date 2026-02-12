40902cd2ade951ec2e95e0641a021a7171b7e6b4
Katya <kotagomon@gmail.com>
2026-02-12 14:21:17 -0800

    Second #2

--- script.js
+++ script.js
@@ -2,4 +2,14 @@
 
 // #1
 const numberOfFilms = +prompt("How many movies have you watched already", "");
-console.log(numberOfFilms);
+//console.log(numberOfFilms);
+
+
+// #2
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
