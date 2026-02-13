6d1d3c3815498e89a5d115a3ceeb7f554b0b7366
Katya <kotagomon@gmail.com>
2026-02-12 16:22:54 -0800

    three #3

--- script.js
+++ script.js
@@ -13,3 +13,14 @@ const personalMovieDB = {
     genres: [],
     privat: false
 };
+
+// #3
+const a = prompt("One of the last movies whatched?", "");
+const b = prompt("How whould you rate the movie", "");
+const c = prompt("One of the last movies whatched?", "");
+const d = prompt("How whould you rate the movie", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
